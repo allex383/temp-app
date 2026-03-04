7c46ecc8a5cf36be05de378f45e39616dd7199aa
allex383 <kino.sigarets@gmail.com>
2026-03-05 01:01:26 +0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import {defineConfig} from 'vite';
+
+export default defineConfig({
+  base: '/temp-app/',
+  plugins: [react(), tailwindcss()],
+  appType: 'spa',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    strictPort: true,
+    hmr: process.env.DISABLE_HMR !== 'true',
+  },
+});
