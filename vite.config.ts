fcfa936d955cbd8d9e4dad6fb8aab4d7ca579ae4
allex383 <kino.sigarets@gmail.com>
2026-03-05 00:10:12 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,14 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  // base должен соответствовать названию вашего репозитория
   base: '/temp-app/',
   plugins: [react(), tailwindcss()],
-  appType: 'spa',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
-    },
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    strictPort: true,
-    hmr: process.env.DISABLE_HMR !== 'true',
-  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
 });
