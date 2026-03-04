db2574d6a4e51a9ae9917edd37d0ea7eac4ed7f1
allex383 <kino.sigarets@gmail.com>
2026-03-05 00:22:15 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // base должен соответствовать названию вашего репозитория
   base: '/temp-app/',
   plugins: [react(), tailwindcss()],
   build: {
