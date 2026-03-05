cfdb72d2be9bbd1717348ab9bef1e51a49b360b8
allex383 <kino.sigarets@gmail.com>
2026-03-05 22:37:27 +0300

    Update vite.config (1).ts

--- vite.config (1).ts
+++ vite.config (1).ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/',
+  root: './',
+  base: './',
   plugins: [
     react({
       fastRefresh: false,
@@ -14,5 +15,11 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 3000,
     strictPort: true,
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html',
+    },
   },
 });
