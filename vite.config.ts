fb0ede966296b30f55d37acd1661230111db0f4a
allex383 <kino.sigarets@gmail.com>
2026-03-05 21:52:48 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
 import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: '/temp-app/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   appType: 'spa',
   resolve: {
