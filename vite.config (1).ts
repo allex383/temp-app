421c571f4f00fef38cbec356730e0f499b2c0545
allex383 <kino.sigarets@gmail.com>
2026-03-05 22:29:57 +0300

    Add files via upload

---
+++ vite.config (1).ts
@@ -0,0 +1,18 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/',
+  plugins: [
+    react({
+      fastRefresh: false,
+    }),
+    tailwindcss(),
+  ],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    strictPort: true,
+  },
+});
