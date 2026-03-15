901d827746429f87b975890800a4d1ec2521858b
allex383 <kino.sigarets@gmail.com>
2026-03-15 21:51:34 +0300

    Add files via upload

---
+++ src/qvData.ts
@@ -0,0 +1,9 @@
+export interface QvEntry {
+  category: string;
+  volumeRange: string;
+  value: number;
+}
+
+export const QV_DATA: QvEntry[] = [
+  // Placeholder data, will be updated with HTML content
+];
