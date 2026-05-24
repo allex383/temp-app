0da62fcc1ff7489d552c702b4f6277db71b4e425
allex383 <kino.sigarets@gmail.com>
2026-05-24 17:27:36 +0300

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -47,7 +47,8 @@ export const Sidebar: React.FC<SidebarProps> = ({ currentView, onNavigate, isOpe
         { id: 'tech-floor', label: 'Теплый пол' },
         { id: 'tech-pool', label: 'Первоначальный нагрев бассейна' },
         { id: 'tech-pool-operating', label: 'Бассейн после промывки фильтра' },
-        { id: 'tech-pool-flow', label: 'Бассейн проточного типа' }
+        { id: 'tech-pool-flow', label: 'Бассейн проточного типа' },
+        { id: 'tech-pool-periodic', label: 'Бассейн с периодической сменой воды' }
       ]
     },
     { id: 'gvs', label: 'ГВС', icon: <Droplets size={18} /> },
